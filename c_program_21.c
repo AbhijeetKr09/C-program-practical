a4613656fd2a0026e99d53f508e829a5afb37cba
ABHIJEET KUMAR <gmrspd@gmail.com>
2023-05-15 21:14:18 +0530

    completed codes program 29 left

--- c_program_21.c
+++ c_program_21.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
-void main()
-{
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
 
+int main() {
+    int arr[] = {5, 1, 4, 2, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
 }
