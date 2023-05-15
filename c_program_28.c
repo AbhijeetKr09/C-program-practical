db7dc7405b07124848bec8d24a5efd463f7ae68c
ABHIJEET KUMAR <gmrspd@gmail.com>
2023-05-15 20:56:39 +0530

    completed codes program 28 left

--- c_program_28.c
+++ c_program_28.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void main()
+int main()
 {
+    FILE *data, *copy_file;
+    char ch;
 
+    data = fopen("file_operation/data.txt", "r");
+    copy_file = fopen("file_operation/copy_file.txt", "w");
+
+    while ((ch = fgetc(data)) != EOF)
+        fputc(ch, copy_file);
+
+    printf("File copied successfully.\n");
+
+    fclose(data);
+    fclose(copy_file);
+
+    return 0;
 }
--- rem.py
+++ rem.py
@@ -1,5 +1,8 @@
 import os
 import time
 for i in range(1, 30):
-    os.remove(f"c_program_{i}.exe")
+    try:
+        os.remove(f"c_program_{i}.exe")
+    except:
+        continue
     time.sleep(1)
